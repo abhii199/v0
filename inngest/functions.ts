f96362d15d046a6543acad82bb7d8a21a7759e81
Zenon <abhishekyadav276208@gmail.com>
2026-03-01 15:48:53 +0530

    Add: terminal tool for command access in sandbox

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,17 +1,60 @@
 import { inngest } from "./client";
 import { Sandbox } from 'e2b'
+import { createAgent, gemini, createTool } from '@inngest/agent-kit'
+import z from "zod";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+export const codeAgentFunction = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
 
   async ({ event, step }) => {
+
+    //step 1: create a sandbox
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("v0-clone-build")
       return sandbox.sandboxId
     })
 
-    await step.sleep("wait-a-moment", "1s");
+    const terminalTool = createTool({
+      name: "terminal",
+      description: "A tool that allows you to run terminal commands in the sandbox. Use this tool to run commands in the terminal and get the output and error.",
+      parameters: z.object({
+        command: z.string().describe("The command to run in the terminal")
+      }),
+      handler: async ({ command }, { step }) => {
+        return await step?.run("terminal", async () => {
+          const buffers = { stdout: "", stderr: "" }
+          try {
+            const sandbox = await Sandbox.connect(sandboxId)
+
+            const result = await sandbox.commands.run(command, {
+              onStdout: (data) => {
+                buffers.stdout += data
+              },
+              onStderr: (data) => {
+                buffers.stderr += data
+              }
+            })
+            return result.stdout
+          } catch (error) {
+            console.log(`Command failed: ${error} \n stdout: ${buffers.stdout} \n stderr: ${buffers.stderr}`)
+            return `Command failed: ${error} \n stdout: ${buffers.stdout} \n stderr: ${buffers.stderr}`
+          }
+        })
+      }
+    })
+
+    const codeAgent = createAgent({
+      name: 'code-agent',
+      description: 'An agent that can run code in a sandbox',
+      system: 'You are a helpful assistant that executes code in a sandbox. You can run code in the sandbox and return the results.',
+      model: gemini({ model: "gemini-1.5-flash-8b" }),
+      tools: [
+        //Terminal
+        //create/update file
+        //read file
+      ]
+    })
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await Sandbox.connect(sandboxId)
       const host = sandbox.getHost(3000)
--- sandbox-templates/next-js/e2b.Dockerfile
+++ sandbox-templates/next-js/e2b.Dockerfile
@@ -12,8 +12,8 @@ WORKDIR /home/user/nextjs-app
 
 RUN npx --yes create-next-app@latest . --yes
 
-RUN npx --yes shadcn@2.6.3 init --yes -b neutral --force
-RUN npx --yes shadcn@2.6.3 add --all --yes
+RUN npx --yes shadcn@latest init --yes -b neutral --force
+RUN npx --yes shadcn@latest add --all --yes
 
 # Move the Nextjs app to the home directory and remove the nextjs-app directory
 RUN mv /home/user/nextjs-app/* /home/user/ && rm -rf /home/user/nextjs-app
